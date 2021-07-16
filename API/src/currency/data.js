4dde641d6be862a5cb6965d6c84994d534932e98
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-07-16 20:23:37 +0500

    updates

---
+++ API/src/currency/data.js
@@ -0,0 +1,16 @@
+const loadCurrency = [
+  {
+    currency: 'USD',
+    name:'dollar',
+    symbol: '$',
+    value: 1
+  },
+  {
+    currency: 'PKR',
+    name:'rupees',
+    symbol: 'Rs',
+    value: 159.0
+  },
+]
+
+module.exports = loadCurrency;
